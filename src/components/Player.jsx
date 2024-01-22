80c23e4975413769aa465a82515c700dede94b41
Zachary Watson <z@justwingit.com>
2024-01-22 09:10:03 -0700

    bound input and button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+
+  const playerNameInput = useRef();
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameInput} type="text" />
+        <button
+          onClick={() => {
+            setPlayerName(playerNameInput.current.value);
+          }}
+        >
+          Set Name
+        </button>
       </p>
     </section>
   );
